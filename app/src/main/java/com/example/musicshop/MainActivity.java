d3d7ab41024e4fa87c36899df800fa3755f343ee
Makarov <v-makarov@yandex.ru>
2021-06-13 20:45:17 +0300

    Добавлена работа с количеством товара

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.musicshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addCountItems(View view) {
+        quantity++;
+        TextView countView = findViewById(R.id.count);
+        countView.setText("" + quantity);
+        //countView.setBackgroundColor(Color.YELLOW);
+        //countView.setTextSize(20);
+    }
+
+    public void decreaseQuantity(View view) {
+        quantity--;
+        TextView countView = findViewById(R.id.count);
+        countView.setText("" + quantity);
     }
 }
